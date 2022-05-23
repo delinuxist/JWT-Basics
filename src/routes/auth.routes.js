be6037b7a4c6281d06266967fab157511691478e
delinuxist <delinuxist.gmail.com>
2022-05-23 01:03:48 +0000

    dashboard, signup and signin added as routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const authControllers = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth");
 
-router.get("/signup", authControllers.signUp);
+router.post("/signup", authControllers.signUp);
+
+router.post("/login", authControllers.signIn);
+
+router.get("/dashboard", authMiddleware, authControllers.dashboard);
 
 module.exports = router;
