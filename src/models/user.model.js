d7237fbb984c1838eefef0f5acf998f545bb1edd
delinuxist <delinuxist.gmail.com>
2022-05-23 01:03:14 +0000

    commented email field

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: {
+  username: {
     type: String,
-    required: [true, "Email required"],
+    required: [true, "Username required"],
   },
+  // email: {
+  //   type: String,
+  //   required: [true, "Email required"],
+  // },
   password: {
     type: String,
     required: [true, "Password required"],
